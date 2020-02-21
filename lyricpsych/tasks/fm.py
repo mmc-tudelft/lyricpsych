53238ccf3da0ea77f16449c20d5d7e235b37caf7
Jaehun Kim <j.h.kim@tudelft.nl>
2020-02-21 11:39:01 +0100

    updated FM

--- lyricpsych/tasks/fm.py
+++ lyricpsych/tasks/fm.py
@@ -1,13 +1,17 @@
 import numpy as np
 import numba as nb
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch import optim
+from torch.utils.data import Dataset, DataLoader
 from torch.nn.utils.rnn import pad_packed_sequence
+
 from tqdm import tqdm
 
 
+# TODO: make decorator for checking the model is already fitted
 class FactorizationMachine(nn.Module):
     def __init__(self, k, init=0.001, n_iters=10, learn_rate=0.001, l2=0.0001,
                  n_negs=10, use_gpu=False):
@@ -18,10 +22,14 @@ class FactorizationMachine(nn.Module):
         self.l2 = l2
         self.n_iters = n_iters
         self.n_negs = n_negs
-        self.target_device = 'gpu' if use_gpu else 'cpu'
+        # TODO: generalization of the device selection (not only for cuda)
+        self.target_device = 'cuda' if use_gpu else 'cpu'
         
     def _init_embeddings(self):
         """"""
+        if not hasattr(self, 'embeddings_'):
+            raise ValueError()
+
         for key, layer in self.embeddings_.items():
             layer.weight.data.copy_(torch.randn(*layer.weight.shape) * self.init)
         
@@ -29,7 +37,7 @@ class FactorizationMachine(nn.Module):
         """
         """
         if not hasattr(self, 'embeddings_'):
-            raise ValueError()
+            raise ValueError('You should call .fit first!')
         
         params = {'sparse':[], 'dense':[]}
         for lyr in self.embeddings_.children():
@@ -57,6 +65,10 @@ class FactorizationMachine(nn.Module):
             
     def _retrieve_factors(self, u, i, feats):
         """"""
+        if not hasattr(self, 'embeddings_'):
+            raise ValueError('You should call .fit first!')
+
+        # retrieve embeddins
         user = self.embeddings_['user'](u)
         item = self.embeddings_['item'](i)
         if self.embeddings_['feat'].sparse:
@@ -64,41 +76,59 @@ class FactorizationMachine(nn.Module):
         else:
             feat = self.embeddings_['feat'].weight[None] * feats[..., None]
         
+        # post-process (devide factor and weight)
         w = torch.cat([user[..., -1], item[..., -1], feat[..., -1]], dim=1)
         v = torch.cat([user[..., :-1], item[..., :-1], feat[..., :-1]], dim=1)
         return w, v
-    
-    def _draw_data(self, u, user_item):
-        i0, i1 = user_item.indptr[u], user_item.indptr[u+1]
-        pos = user_item.indices[i0:i1]
-        j0 = np.random.choice(len(pos))
-        negs = negsamp_vectorized_bsearch(
-            pos, user_item.shape[1], n_samp=self.n_negs
-        )
-        return pos[j0][None], negs
-    
-    def preproc(self, u, pos, negs):
-        """"""
-        u = torch.full((self.n_negs + 1,), u).long().to(self.target_device)
-        i = torch.LongTensor(np.r_[pos, negs]).to(self.target_device)
-        y = torch.LongTensor([1] + [-1] * self.n_negs)
-        return u[:, None], i[:, None], y
-    
-    def _draw_batch(self, user_item, item_feature, batch_sz):
-        """"""
-        U, I, Y, X = [], [], [], []
-        for u in np.random.choice(user_item.shape[0], batch_sz, False): 
-            pos, negs = self._draw_data(u, user_item)
-            u, i, y = self.preproc(u, pos, negs)
-            x = item_feature[i[:, 0]].to(self.target_device)
 
-            U.append(u)
-            I.append(i)
-            Y.append(y)
-            X.append(x)
+    def _update_z(self, item_feature, verbose=True):
+        """
+        this method pre-compute & cache item-tag factor for prediction
+        """
+        if not hasattr(self, 'embeddings_'):
+            raise ValueError('You should call .fit first!')
+
+        n_items = item_feature.shape[0]
+        Z = torch.zeros((item_feature.shape[0], self.k+1))
+        Z2 = torch.zeros((item_feature.shape[0], self.k))
+        for i in tqdm(range(n_items), disable=not verbose, ncols=80):
             
-        return (torch.cat(U, dim=0), torch.cat(I, dim=0),
-                torch.cat(Y, dim=0), torch.cat(X, dim=0))
+            item = torch.LongTensor([i])
+            feat = item_feature[i]
+
+            z = [self.embeddings_['item'](item).detach()]
+            if self.embeddings_['feat'].sparse:
+                z += [self.embeddings_['feat'](feat).detach()]
+            else:
+                z += [
+                    (self.embeddings_['feat'].weight * feat[..., None]).detach()
+                ]
+            z = torch.cat(z)
+
+            Z[i] = z.sum(0)
+            Z2[i] = (z[..., :-1]**2).sum(0)
+                
+        # register
+        self.register_buffer('Z_v', Z[..., :-1])
+        self.register_buffer('Z_w', Z[..., -1][..., None])
+        self.register_buffer('Z2_v', Z2)
+
+    def predict_user(self, user):
+        if not hasattr(self, 'embeddings_'):
+            raise ValueError('You should call .fit first!')
+
+        user = torch.LongTensor([user]).to(self.target_device)
+        user = self.embeddings_['user'](user)
+        u_w = user[..., -1][..., None]  # (bsz, 1)
+        u_v = user[..., :-1]  # (bsz, k)
+        
+        # infer
+        w = u_w[:, None] + self.Z_w[None]
+        v = u_v[:, None] + self.Z_v[None]
+        v2 = u_v[:, None]**2 + self.Z2_v[None]
+        
+        s = self.w0 + w[:, 0] + (v**2 - v2).sum(-1) * .5
+        return s[0]
         
     def forward(self, u, i, feats):
         """
@@ -107,17 +137,24 @@ class FactorizationMachine(nn.Module):
         feats (torch.FloatTensor): item feature tensor
         """
         if not hasattr(self, 'embeddings_'):
-            raise ValueError()
+            raise ValueError('You should call .fit first!')
             
         w, v = self._retrieve_factors(u, i, feats)
-        w = w.sum(1)
-        v = (v.sum(1)**2 - (v**2).sum(1)).sum(-1) * .5
-        s = self.w0 + w + v
-        return s
+        w_ = w.sum(1)
+        v_ = (v.sum(1)**2 - (v**2).sum(1)).sum(-1) * .5
+        s = self.w0 + w_ + v_
+        return s, [w, v]
 
     def fit(self, user_item, item_feature, feature_sparse=False,
-            report_every=2000, batch_sz=512, verbose=False):
+            report_every=2000, batch_sz=512, verbose=False, n_jobs=4):
         """"""
+        # TODO: internal validation if specified
+
+        # set some variables
+        feats = torch.Tensor(item_feature)  # convert feature to the torch Tensor
+        n_users, n_items = user_item.shape
+        desc_tmp = '[tloss=0.0000]'
+
         # initialize the factors
         self.register_parameter('w0', nn.Parameter(torch.FloatTensor([0])))
         self.embeddings_ = nn.ModuleDict({
@@ -127,52 +164,59 @@ class FactorizationMachine(nn.Module):
                                  self.k + 1, sparse=feature_sparse)
         })
         self._init_embeddings()
+        if self.target_device != 'cpu':
+            self.to(self.target_device)
+        
+        # prepare dataset
+        db = RecFeatDataset(user_item, n_negs=self.n_negs,
+                            item_feature=item_feature)
+        dl = DataLoader(db, batch_sz, collate_fn=collate_triplets_with_feature,
+                        shuffle=True, num_workers=n_jobs, drop_last=True)
         
         # init optimizer
         self._init_optimizer()
-                
+
         # do the optimization
-        self.losses_ = []
-        feats = torch.Tensor(item_feature)
-#         with tqdm(total=self.n_iters, ncols=80, disable=not verbose) as p:
-        for i in range(self.n_iters):
-            loss_sum = 0.
-            j = 0
-            n_users, n_items = user_item.shape
-            desc_tmp = '[tloss=0.00000000]'
-            rng = np.arange(0, n_users, batch_sz)
-            with tqdm(total=len(rng), desc=desc_tmp,
-                      ncols=80, disable=not verbose) as prog:
-                for i in rng:
-                    # draw batch
-                    u, i, y, x = self._draw_batch(user_item, feats, batch_sz)
+        try:
+            for i in range(self.n_iters):
+                with tqdm(total=len(dl), desc=desc_tmp,
+                        ncols=80, disable=not verbose) as prog:
+                    for batch in dl:
+                        # send batch to the device
+                        u, i, y, x = tuple([x.to(self.target_device) for x in batch])
 
-#                     # flush grads
-#                     self.opt.zero_grad()
+                        # flush grads
+                        self.opt.zero_grad()
 
-#                     # forward & compute loss
-#                     s = self.forward(u, i, x) * y
-#                     loss = -F.logsigmoid(s).sum()
+                        # forward & compute loss
+                        scores, weights = self.forward(u, i, x)
 
-#                     # backward
-#                     loss.backward()
+                        # compute the main loss
+                        # TODO: generalize loss function
+                        z = scores * y
+                        loss = -F.logsigmoid(z).sum()
 
-#                     # update params
-#                     self.opt.step()
+                        # adding L2 regularization term for sparse entries (user/item)
+                        loss += self.l2 * sum([torch.sum(w[:,:2]**2) for w in weights])
 
-#                     # update loss and counter
-#                     prog.set_description('[tloss={:.8f}]'.format(
-#                         loss.item()/batch_sz))
-#                     loss_sum += loss
-#                     j += 1
-#                     if (j + 1) % report_every == 0:
-#                         self.losses_.append(
-#                             (loss_sum.data.numpy() / report_every)
-#                         )
-#                         loss_sum = 0
-#                         j = 0
-                    prog.update(1)
-        
+                        # backward
+                        loss.backward()
+
+                        # update params
+                        self.opt.step()
+
+                        # update loss and counter
+                        prog.set_description('[tloss={:.4f}]'.format(
+                            loss.item()/batch_sz))
+                        prog.update(1)
+        except KeyboardInterrupt as e:
+            print('User stopped the training!...')
+        finally:
+            # update the cached factors for faster inference
+            self.cpu()  # since below process eats up lots of memory
+            self._update_z(feats.to('cpu'))
+            self.to(self.target_device)
+
     
 class MultipleOptimizer:
     """ Simple wrapper for the multiple optimizer scenario """
@@ -198,6 +242,63 @@ class MultipleOptimizer:
         """"""
         return [op.load_state_dict(state) for state in state_dict]
     
+
+class RecFeatDataset(Dataset):
+    def __init__(self, user_item, user_feature=None, item_feature=None,
+                 n_negs=10, device='cpu'):
+        super().__init__()
+        self.user_item = user_item
+        self.user_feature = user_feature
+        self.item_feature = item_feature
+        self.n_negs = n_negs
+        self.device = device
+
+    def _draw_data(self, u, user_item):
+        i0, i1 = user_item.indptr[u], user_item.indptr[u+1]
+        pos = user_item.indices[i0:i1]
+        j0 = np.random.choice(len(pos))
+        negs = negsamp_vectorized_bsearch(
+            pos, user_item.shape[1], n_samp=self.n_negs
+        )
+        return pos[j0][None], negs
+    
+    def _preproc(self, u, pos, negs):
+        """"""
+        u = torch.full((self.n_negs + 1,), u).long().to(self.device)
+        i = torch.LongTensor(np.r_[pos, negs]).to(self.device)
+        y = torch.LongTensor([1] + [-1] * self.n_negs).to(self.device)
+        return u[:, None], i[:, None], y
+    
+    def _draw_batch(self, user_item, item_feature, batch_sz):
+        """"""
+        U, I, Y, X = [], [], [], []
+        for u in np.random.choice(user_item.shape[0], batch_sz, False): 
+            pos, negs = self._draw_data(u, user_item)
+            u, i, y = self.preproc(u, pos, negs)
+            x = item_feature[i[:, 0]].to(self.target_device)
+
+            U.append(u)
+            I.append(i)
+            Y.append(y)
+            X.append(x)
+            
+        return (torch.cat(U, dim=0), torch.cat(I, dim=0),
+                torch.cat(Y, dim=0), torch.cat(X, dim=0))
+
+    def __len__(self):
+        return self.user_item.shape[0]
+    
+    def __getitem__(self, u_idx):
+        pos, negs = self._draw_data(u_idx, self.user_item)
+        u, i, y = self._preproc(u_idx, pos, negs)
+        x = torch.FloatTensor(self.item_feature[i[:, 0]]).to(self.device)
+        return u, i, y, x
+
+
+def collate_triplets_with_feature(samples):
+    """"""
+    return tuple(map(torch.cat, zip(*samples)))
+
 
 @nb.njit("i8[:](i4[:], i8, i8)")
 def negsamp_vectorized_bsearch(pos_inds, n_items, n_samp=32):
